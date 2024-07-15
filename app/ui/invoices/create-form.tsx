a92638d5b3660310effc248756020666a4063728
Irshal Firdeansyah <irshalfir@gmail.com>
2024-07-16 04:03:28 +0700

    create invoice

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,3 +1,4 @@
+import { createInvoice } from "@/app/lib/actions";
 import { CustomerField } from '@/app/lib/definitions';
 import { Button } from '@/app/ui/button';
 import { CheckIcon, ClockIcon, CurrencyDollarIcon, UserCircleIcon, } from '@heroicons/react/24/outline';
@@ -5,7 +6,7 @@ import Link from 'next/link';
 
 export default function Form({customers}: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
