aef7035895b59ec33647615172687d0c93e1e6ed
Irshal Firdeansyah <irshalfir@gmail.com>
2024-07-16 06:50:36 +0700

    implement update invoice

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -4,6 +4,7 @@ import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import { Button } from '@/app/ui/button';
 import { CheckIcon, ClockIcon, CurrencyDollarIcon, UserCircleIcon, } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { updateInvoice } from '@/app/lib/actions'
 
 export default function EditInvoiceForm({
                                           invoice,
@@ -12,8 +13,10 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
